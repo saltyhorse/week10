618703d5efd0dac710086b723d759863e7050450
Brian Eng <b-eng@kellogg.northwestern.edu>
2020-06-03 16:43:57 +0000

    initial commit

---
+++ app.rb
@@ -0,0 +1,105 @@
+# Set up for the application and database. DO NOT CHANGE. #############################
+require "sinatra"  
+require "sinatra/cookies"                                                             #
+require "sinatra/reloader" if development?                                            #
+require "sequel"                                                                      #
+require "logger"                                                                      #
+require "bcrypt"                                                                      #
+connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
+DB ||= Sequel.connect(connection_string)                                              #
+DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
+def view(template); erb template.to_sym; end                                          #
+use Rack::Session::Cookie, key: 'rack.session', path: '/', secret: 'secret'           #
+before { puts; puts "--------------- NEW REQUEST ---------------"; puts }             #
+after { puts; }                                                                       #
+#######################################################################################
+
+events_table = DB.from(:events)
+rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+
+before do
+    # SELECT * FROM users WHERE id = session[:user_id]
+    @current_user = users_table.where(:id => session[:user_id]).to_a[0]
+    puts @current_user.inspect
+end
+
+# Home page (all events)
+get "/" do
+    # before stuff runs
+    @events = events_table.all
+    view "events"
+end
+
+# Show a single event
+get "/events/:id" do
+    @users_table = users_table
+    # SELECT * FROM events WHERE id=:id
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    # SELECT * FROM rsvps WHERE event_id=:id
+    @rsvps = rsvps_table.where(:event_id => params["id"]).to_a
+    # SELECT COUNT(*) FROM rsvps WHERE event_id=:id AND going=1
+    @count = rsvps_table.where(:event_id => params["id"], :going => true).count
+    view "event"
+end
+
+# Form to create a new RSVP
+get "/events/:id/rsvps/new" do
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    view "new_rsvp"
+end
+
+# Receiving end of new RSVP form
+post "/events/:id/rsvps/create" do
+    rsvps_table.insert(:event_id => params["id"],
+                       :going => params["going"],
+                       :user_id => @current_user[:id],
+                       :comments => params["comments"])
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    view "create_rsvp"
+end
+
+# Form to create a new user
+get "/users/new" do
+    view "new_user"
+end
+
+# Receiving end of new user form
+post "/users/create" do
+    users_table.insert(:name => params["name"],
+                       :email => params["email"],
+                       :password => params["password"])
+    view "create_user"
+end
+
+# Form to login
+get "/logins/new" do
+    view "new_login"
+end
+
+# Receiving end of login form
+post "/logins/create" do
+    puts params
+    email_entered = params["email"]
+    password_entered = params["password"]
+    # SELECT * FROM users WHERE email = email_entered
+    user = users_table.where(:email => email_entered).to_a[0]
+    if user
+        puts user.inspect
+        # test the password against the one in the users table
+        if user[:password] == password_entered
+            session[:user_id] = user[:id]
+            view "create_login"
+        else
+            view "create_login_failed"
+        end
+    else 
+        view "create_login_failed"
+    end
+end
+
+# Logout
+get "/logout" do
+    session[:user_id] = nil
+    view "logout"
+end
